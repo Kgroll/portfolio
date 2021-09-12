71693a33c6d68805382565ea79a62057ec724eb8
Kristen Groller <groller@rogers.com>
2021-09-12 17:18:35 -0400

    removed map

--- assets/js/script.js
+++ assets/js/script.js
@@ -167,7 +167,7 @@ function scroll() {
   // google map
   ///////////////////////////////
 
-function initialize()
+/*function initialize()
 {
 var mapProp = {
   center:new google.maps.LatLng(51.508742,-0.120850),
@@ -180,6 +180,6 @@ var map=new google.maps.Map(document.getElementById("googleMap")
   ,mapProp);
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', initialize);*/
 
 
